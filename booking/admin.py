9881817618aa4d8cfdbf7363fa2acd3091bdc87d
Ammar <mohamedammar6650@gmail.com>
2025-08-28 01:03:15 +0800

    Updated

--- booking/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Venue, Booking
-
-admin.site.register(Venue)
-admin.site.register(Booking)
--- venue/admin.py
+++
@@ -1,7 +0,0 @@
-
-# Register your models here.
-from django.contrib import admin
-from .models import Venue, Booking
-
-admin.site.register(Venue)
-admin.site.register(Booking)
